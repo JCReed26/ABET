edfc789a455854e0174f163e5569ab65b09f0a9d
JCReed26 <117855743+JCReed26@users.noreply.github.com>
2025-02-07 20:42:57 -0500

    alot of changes here the backend is no longer going to handle with mysql i will use that to make an api for the reader and categorization as well as ml work for backend so it itself can be separate from the database. Insert supabase

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,15 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-#need to make sure we can CRUD into the database on mySQL also how can we view the data in workbench
-#loads the environment creates the database engine and creates database session
-#port 3306
-load_dotenv()
-import os
-DB_URL = os.getenv('http://localhost:3306')
-engine = create_engine(DB_URL, echo=True)
+MySQLdb_URL = 'mysql://root:shawty@localhost:3306/abet_users'
+
+engine = create_engine(MySQLdb_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 
-from . import crud, models, schema
+# Fix imports to be relative
 from .database import SessionLocal, engine
+from . import models
 
+#create database
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -16,23 +18,14 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/users/", response_model=schema.User)
-def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.create_user(db, user)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already exists")
-    return crud.create_user(db, user)
-
-@app.get("/users/{user_id}", response_model=schema.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    user = crud.get_users(db, user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+@app.post("/users/")
+def create_user(name: str, email: str, db: Session = Depends(get_db)):
+    user = models.User(name=name, email=email)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
     return user
 
-@app.get("/users/email/{email}", response_model=schema.User)
-def read_user_by_email(email: str, db: Session = Depends(get_db)):
-    user = crud.get_user_by_email(db, email)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+@app.get("/users/")
+def get_users(db: Session = Depends(get_db)):
+    return db.query(models.User).all()
