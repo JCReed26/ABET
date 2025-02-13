5e0a3bbf1c134de5799be0c94b09f969734eb1ef
JCReed26 <117855743+JCReed26@users.noreply.github.com>
2025-02-13 00:10:11 -0500

    added receipt image upload processing not yet tested

--- frontend/src/views/receipt-processor.js
+++ frontend/src/views/receipt-processor.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 function ReceiptProcessor() {
 
-    const handleFileUpload = (event) => {
+    const [receipt_data, setReceiptData] = useState([]);
+
+
+    const handleFileUpload = async (event) => {
         const file = event.target.files[0]; 
         console.log(file); 
+    }
+
+    const uploadReceipt = async (image) => {
+        // gets image from file upload
+        const formData = new FormData(); 
+        formData.append('file', image);
+
+        //try api request
+        try {
+            const response = await fetch('http://localhost:8000/process_image')
+                .then(response => response.json())
+                .then(response => console.log(response))
+                .then(response => setReceiptData(response))
+        } catch (error) {
+            throw new Error("Failed to process image");
+            console.log(error);
+        }
     }
 
     return (
@@ -16,6 +36,10 @@ function ReceiptProcessor() {
                 <div className=''>
                     <input type='file' id='file-input' accept='.jpg, .png, .jpeg' className='form-control' onChange={handleFileUpload} />
                 </div>
+            </div>
+            <div className="json-visualizer">
+                <h2>JSON Output</h2>
+                {receipt_data && <pre>{JSON.stringify(receipt_data, null, 2)}</pre>}
             </div>
         </div>
     );
