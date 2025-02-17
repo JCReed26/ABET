69ecd7aaa76ed90e7a20bc273d873a506b529884
JCReed26 <117855743+JCReed26@users.noreply.github.com>
2025-02-17 01:57:22 -0500

    made income page @ bare level

--- frontend/src/views/income.js
+++ frontend/src/views/income.js
@@ -2,15 +2,30 @@ import { useState, useEffect } from 'react';
 import supabase from '../supabase-client';
 import '../styles/income.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPencilAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faPencilAlt, faTrashAlt, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function Incomes() {
 
+  //overall data and functions 
   const [income_data, setIncomeData] = useState([]);
-  const [income_name, setIncomeName] = useState(''); 
-  const [income_amount, setIncomeAmount] = useState('0');
-  const [income_type, setIncomeType] = useState('');
-  
+
+  const [IsCreatePopUpOpen, setIsCreatePopUpOpen] = useState(false);
+  const [create_data, setCreateData] = useState({
+    id: null,
+    name: '', 
+    amount: '',
+    type: ''
+  });
+
+  const [IsUpdatePopUpOpen, setIsUpdatePopUpOpen] = useState(false);
+  const [update_data, setUpdateData] = useState({
+    id: null,
+    name: '', 
+    amount: '',
+    type: ''
+  });
+
+  //read functionality 
   useEffect(() => {
     getIncome();
   }, []);
@@ -30,32 +45,44 @@ function Incomes() {
     }
   }
   
-  
+  const handleSubmitClick = () => {
+    setCreateData({
+      name: '',
+      amount: '',
+      type: ''
+    });
+    setIsCreatePopUpOpen(true);
+  }
+
+  //create functionality 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      if (!income_name || !income_amount || !income_type) {
+      if (!create_data.name || !create_data.amount || !create_data.type) {
         alert('Please fill out all fields');
         return;
       }
 
-      const { data, error } = await supabase 
+      const { error } = await supabase 
         .from('income')
         .insert([{
-          name: income_name,
-          amount: income_amount, 
-          type: income_type
+          name: create_data.name,
+          amount: create_data.amount, 
+          type: create_data.type
         }]);
       
       if (error) throw error; 
 
       //refresh 
-      getIncome(data || []);
+      getIncome();
 
       //clear form 
-      setIncomeName('');
-      setIncomeAmount('');
-      setIncomeType(''); 
+      setIsCreatePopUpOpen(false);
+      setCreateData({
+        name: '',
+        amount: '',
+        type: ''
+      });
 
     } catch (error) {
       console.error('Error:', error.message);
@@ -63,6 +90,7 @@ function Incomes() {
     }
   }
 
+  //delete functionality 
   const handleDelete = async (id) => {
     try {
       const { data, error } = await supabase 
@@ -80,41 +108,46 @@ function Incomes() {
     }
   }
 
+  //update functionalities 
+  const handleUpdateClick = (data) => {
+    setUpdateData({
+      id: data.id,
+      name: data.name, 
+      amount: data.amount, 
+      type: data.type
+    });
+    setIsUpdatePopUpOpen(true);
+  };
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const {error} = await supabase 
+        .from('income')
+        .update({
+          name: update_data.name,
+          amount: update_data.amount,
+          type: update_data.type
+        })
+        .eq('id', update_data.id);
+
+      if (error) throw error; 
+
+      setIsUpdatePopUpOpen(false);
+      getIncome();
+    } catch (error) {
+      console.error('Error:', error.message);
+      alert('error updating item');
+    }
+  };
 
 
   return (
     <div>
-        <div>
-          <h1>Incomes</h1>
-          <form onSubmit={handleSubmit}>
-            <label> 
-              Name: 
-              <input 
-                type='text'
-                value={income_name}
-                onChange={(e) => setIncomeName(e.target.value)}
-              />
-            </label>
-            <label> 
-              Amount: 
-              <input 
-                type='text'
-                value={income_amount}
-                onChange={(e) => setIncomeAmount(e.target.value)}
-              />
-            </label>
-            <label> 
-              Type: 
-              <input 
-                type='text'
-                value={income_type}
-                onChange={(e) => setIncomeType(e.target.value)}
-              />
-            </label>
-            <input type='submit' value="Submit" />
-          </form>
-        </div>
-        <div>
+      <div>
+        <h1>Incomes</h1>
+      </div>
+      <div>
           <h2>Income Data</h2>
           <ul>
             {income_data.length === 0 ? (
@@ -124,7 +157,7 @@ function Incomes() {
                 <li key={index}>
                   {data.type} - {data.name} - ${data.amount}
                   <div className='action-button'>
-                    <button className='icon-button edit-button'>
+                    <button className='icon-button edit-button' onClick={() => handleUpdateClick(data)}>
                       <FontAwesomeIcon icon={faPencilAlt} />
                     </button>
                     <button className='icon-button delete-button' onClick={() => handleDelete(data.id)}>
@@ -135,7 +168,91 @@ function Incomes() {
               ))
             )}
           </ul>
+          <button className='add-income-button' onClick={handleSubmitClick}>
+            <span>Add New Income</span>
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+      </div>
+
+      <div>
+        {IsCreatePopUpOpen && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <h2>Add Income</h2>
+              <form onSubmit={handleSubmit}>
+                <label>
+                  Name:
+                  <input
+                    type="text"
+                    value={create_data.name}
+                    onChange={(e) => setCreateData({...create_data, name: e.target.value})}
+                  />
+                </label>
+                <label>
+                  Amount:
+                  <input
+                    type="text"
+                    value={create_data.amount}
+                    onChange={(e) => setCreateData({...create_data, amount: e.target.value})}
+                  />
+                </label>
+                <label>
+                  Type:
+                  <input
+                    type="text"
+                    value={create_data.type}
+                    onChange={(e) => setCreateData({...create_data, type: e.target.value})}
+                  />
+                </label>
+                <div className="popup-buttons">
+                  <button type="submit">Add</button>
+                  <button type="button" onClick={() => setIsCreatePopUpOpen(false)}>Cancel</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div>
+        {IsUpdatePopUpOpen && (
+        <div className="popup-overlay">
+          <div className="popup">
+            <h2>Update Income</h2>
+            <form onSubmit={handleUpdate}>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  value={update_data.name}
+                  onChange={(e) => setUpdateData({...update_data, name: e.target.value})}
+                />
+              </label>
+              <label>
+                Amount:
+                <input
+                  type="text"
+                  value={update_data.amount}
+                  onChange={(e) => setUpdateData({...update_data, amount: e.target.value})}
+                />
+              </label>
+              <label>
+                Type:
+                <input
+                  type="text"
+                  value={update_data.type}
+                  onChange={(e) => setUpdateData({...update_data, type: e.target.value})}
+                />
+              </label>
+              <div className="popup-buttons">
+                <button type="submit">Update</button>
+                <button type="button" onClick={() => setIsUpdatePopUpOpen(false)}>Cancel</button>
+              </div>
+            </form>
+          </div>
         </div>
+        )}
+      </div>
     </div>
   );
 }
