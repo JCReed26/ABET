f3b675292c8a16c43160f321b5f705739a9bea68
JCReed26 <117855743+JCReed26@users.noreply.github.com>
2025-02-18 11:30:42 -0500

    types and functions created for the pop ups return html pop ups not yet created

--- frontend/src/views/expense.js
+++ frontend/src/views/expense.js
@@ -8,6 +8,22 @@ function Expenses() {
 
   const [expenses, setExpenses] = useState([]);
 
+  const [IsCreatePopUpOpen, setIsCreatePopUpOpen] = useState(false); 
+  const [create_data, setCreateData] = useState({
+    id: null,
+    name: '',
+    amount: 0.0, 
+    category: ''
+  });
+
+  const [IsUpdatePopUpOpen, setIsUpdatePopUpOpen] = useState(false); 
+  const [update_data, setUpdateData] = useState({
+    id: null,
+    name: '',
+    amount: 0.0, 
+    category: ''
+  });
+
 
   useEffect(() => {
     getExpenses();
@@ -46,6 +62,82 @@ function Expenses() {
     }
   }
 
+  const handleNewExpense = async (e) => {
+    e.preventDefault();
+    try {
+      if (!create_data.name || !create_data.amount || !create_data.type) {
+        alert('Please fill out all fields');
+        return;
+      }
+
+      const { error } = await supabase 
+        .from('expenses')
+        .insert([{
+          name: create_data.name,
+          amount: create_data.amount, 
+          type: create_data.type
+        }]);
+      
+      if (error) throw error; 
+
+      //refresh 
+      getIncome();
+
+      //clear form 
+      setIsCreatePopUpOpen(false);
+      setCreateData({
+        name: '',
+        amount: 0.0,
+        category: ''
+      });
+
+    } catch (error) {
+      console.error('Error:', error.message);
+      alert('Error adding expense');
+    }
+  };
+
+  const handleNewExpClick = (data) => {
+    setCreateData({
+      name: '',
+      amount: 0.0, 
+      category: ''
+    });
+    setIsCreatePopUpOpen(true)
+  };
+
+  const handleUpdateExpense = async (e) => {
+    e.preventDefault();
+    try {
+      const {error} = await supabase 
+        .from('expenses')
+        .update({
+          name: update_data.name,
+          amount: update_data.amount,
+          category: update_data.category
+        })
+        .eq('id', update_data.id);
+
+      if (error) throw error; 
+      getExpenses();
+      setIsUpdatePopUpOpen(false);
+      
+    } catch (error) {
+      console.error('Error:', error.message);
+      alert('error updating item');
+    }
+  };
+
+  const handleUpdateClick = (data) => {
+    setUpdateData({
+      id: data.id,
+      name: data.name || '',
+      amount: data.amount || 0.0, 
+      category: data.category || ''
+    });
+    setIsUpdatePopUpOpen(true);
+  }; 
+
 
   return (
     <div>
@@ -63,7 +155,7 @@ function Expenses() {
                 <li key={index}>
                   <span><FontAwesomeIcon icon={faMoneyBills}/> {index} - {data.name} - ${data.amount} - {data.category}</span>
                   <div className='action-button'>
-                    <button className='icon-button edit-button'>
+                    <button className='icon-button edit-button' onClick={handleUpdateExpense}>
                       <FontAwesomeIcon icon={faPencilAlt}/>
                     </button>
                     <button className='icon-button delete-button' onClick={() => handleDelete(data.id)}>
@@ -75,11 +167,17 @@ function Expenses() {
             )
           }
         </ul>
-        <button className='add-income-button'>
+        <button className='add-income-button' onClick={handleNewExpense}>
           <span>Add New Expense</span>
           <FontAwesomeIcon icon={faPlus} /> 
         </button>
       </div>
+
+        
+
+
+
+
     </div>
 );
 }
