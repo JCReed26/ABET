9931568acde0a79e2b56d892f9b9e84495db9a18
JCReed26 <117855743+JCReed26@users.noreply.github.com>
2025-02-23 16:14:40 -0500

    should change it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Sidebar from "./components/sidebar"; 
 import {
-  BrowserRouter as Router, 
+  HashRouter as Router, 
   Routes, 
   Route,
 } from "react-router-dom";
